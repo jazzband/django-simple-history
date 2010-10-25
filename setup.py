e8d2a58dd600bbe4f1d2bac77c684ca9426b2f38
Corey Bertram <corey@qr7.com>
2010-10-25 18:44:19 -0400

    Filled out setup.py and got it to a working state, package now includes all files needed to build/install. included an .hgignore and MANIFEST.in file as well

--- setup.py
+++ setup.py
@@ -1,6 +1,36 @@
 from distutils.core import setup
+import os
+
+# compile the list of packages available, because distutils doesn't have an easy way to do this
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('simple_history'):
+    # ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pgk.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        # strip 'simple_history/' or 'simple_history\'
+        prefix = dirpath[15:]
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
 setup(name='simple_history',
       version='1.0',
-      py_modules=['simple_history'],
+      description='Store Django model history with the ability to revert back to a specific change at any time.',
+      author='Corey Bertram',
+      author_email='corey@qr7.com',
+      url='http://bitbucket.org/q/django-simple-history',
+      package_dir={'simple_history': 'simple_history'},
+      packages=packages,
+      package_data={'simple_history': data_files},
       )
 
