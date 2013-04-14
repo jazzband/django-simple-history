79433520ed93029cc803417d5ccd4a4fcb1464bf
Trey Hunner <trey@treyhunner.com>
2013-04-14 00:50:16 -0700

    Fixup setup.py file (change names and formatting)

--- setup.py
+++ setup.py
@@ -1,39 +1,14 @@
-from setuptools import setup
-import os
+from setuptools import setup, find_packages
 
-# compile the list of packages available, because distutils doesn't have an easy way to do this
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
 
-for dirpath, dirnames, filenames in os.walk('simple_history'):
-    # ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        # strip 'simple_history/' or 'simple_history\'
-        prefix = dirpath[15:]
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-setup(name='simple_history',
-      version='1.1.3',
+setup(name='django-simple-history',
+      version='1.1.3.post1',
       description='Store Django model history with the ability to revert back to a specific change at any time.',
       author='Corey Bertram',
       author_email='corey@qr7.com',
-      mantainer='Joao Pedro Francese',
-      mantainer_email='joaofrancese@gmail.com',
-      url='http://bitbucket.org/joaofrancese/django-simple-history',
-      package_dir={'simple_history': 'simple_history'},
-      packages=packages,
-      package_data={'simple_history': data_files},
+      mantainer='Trey Hunner',
+      url='https://github.com/treyhunner/django-simple-history',
+      packages=find_packages(),
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Framework :: Django",
