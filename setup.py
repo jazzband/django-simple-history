c8e43ba1a9cb2749f7d35eacb8d39aee9ff90e6c
Trey Hunner <trey@treyhunner.com>
2013-04-14 02:09:58 -0700

    Restructure setup.py file

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 
 
-setup(name='django-simple-history',
-      version='1.1.3.post1',
-      description='Store Django model history with the ability to revert back to a specific change at any time.',
-      author='Corey Bertram',
-      author_email='corey@qr7.com',
-      mantainer='Trey Hunner',
-      url='https://github.com/treyhunner/django-simple-history',
-      packages=find_packages(),
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Framework :: Django",
-          ],
+setup(
+    name='django-simple-history',
+    version='1.1.3.post1',
+    description='Store Django model history with the ability to revert back to a specific change at any time.',
+    author='Corey Bertram',
+    author_email='corey@qr7.com',
+    mantainer='Trey Hunner',
+    url='https://github.com/treyhunner/django-simple-history',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+    ],
     tests_require=["Django>=1.2"],
     test_suite='runtests.main',
 )
