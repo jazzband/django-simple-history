0b3175ea97f85dc1a4c92db9f1dc6d5dcca34f95
matklad <aleksey.kladov@gmail.com>
2013-05-16 19:37:22 +0400

    use custom user for tests if django.VERSION >= 1.5

--- runtests.py
+++ runtests.py
@@ -3,6 +3,7 @@ import sys
 from shutil import rmtree
 from os.path import abspath, dirname, join
 
+import django
 from django.conf import settings
 
 
@@ -12,26 +13,32 @@ sys.path.insert(0, abspath(dirname(__file__)))
 if not settings.configured:
     media_root = join(abspath(dirname(__file__)), 'test_files')
     rmtree(media_root, ignore_errors=True)
+
+    installed_apps = (
+        'django.contrib.contenttypes',
+        'django.contrib.auth',
+        'django.contrib.sessions',
+        'django.contrib.admin',
+        'simple_history',
+        'simple_history.tests',
+        'simple_history.tests.external',
+    )
+    auth_user_model = 'auth.User'
+    if django.VERSION >= (1, 5):
+        installed_apps += ('simple_history.tests.custom_user', )
+        auth_user_model = 'custom_user.CustomUser'
+
     settings.configure(
         ROOT_URLCONF='simple_history.tests.urls',
         MEDIA_ROOT=media_root,
         STATIC_URL='/static/',
-        INSTALLED_APPS=(
-            'django.contrib.contenttypes',
-            'django.contrib.auth',
-            'django.contrib.sessions',
-            'django.contrib.admin',
-            'simple_history',
-            'simple_history.tests',
-            'simple_history.tests.external',
-            # 'simple_history.tests.custom_user'
-        ),
-        # AUTH_USER_MODEL='custom_user.CustomUser',
+        INSTALLED_APPS=installed_apps,
         DATABASES={
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
             }
         },
+        AUTH_USER_MODEL=auth_user_model
     )
 
 
