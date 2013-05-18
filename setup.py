63f5e162678fab786d2c369161e842ff7e336105
Trey Hunner <trey@treyhunner.com>
2013-05-17 23:05:45 -0700

    Bump version (post 1.3.0 release)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-simple-history',
-    version='1.3.0',
+    version='1.3.0.post1',
     description='Store model history and view/revert changes from admin site.',
     long_description='\n'.join((
         open('README.rst').read(),
