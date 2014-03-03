547acfbbeb8fd45e816a8e8c680226256fa55b31
Trey Hunner <trey@treyhunner.com>
2014-03-03 14:32:14 -0800

    Run django.setup() in runtests

--- runtests.py
+++ runtests.py
@@ -10,39 +10,42 @@ from django.conf import settings
 sys.path.insert(0, abspath(dirname(__file__)))
 
 
-if not settings.configured:
-    media_root = join(abspath(dirname(__file__)), 'test_files')
-    rmtree(media_root, ignore_errors=True)
+media_root = join(abspath(dirname(__file__)), 'test_files')
+rmtree(media_root, ignore_errors=True)
 
-    installed_apps = (
-        'django.contrib.contenttypes',
-        'django.contrib.auth',
-        'django.contrib.sessions',
-        'django.contrib.admin',
-        'simple_history',
-        'simple_history.tests',
-        'simple_history.tests.external',
-    )
-    auth_user_model = 'auth.User'
-    if django.VERSION >= (1, 5):
-        installed_apps += ('simple_history.tests.custom_user', )
-        auth_user_model = 'custom_user.CustomUser'
+installed_apps = (
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'simple_history',
+    'simple_history.tests',
+    'simple_history.tests.external',
+)
+auth_user_model = 'auth.User'
+if django.VERSION >= (1, 5):
+    installed_apps += ('simple_history.tests.custom_user', )
+    auth_user_model = 'custom_user.CustomUser'
 
-    settings.configure(
-        ROOT_URLCONF='simple_history.tests.urls',
-        MEDIA_ROOT=media_root,
-        STATIC_URL='/static/',
-        INSTALLED_APPS=installed_apps,
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-            }
-        },
-        AUTH_USER_MODEL=auth_user_model
-    )
+DEFAULT_SETTINGS = dict(
+    ROOT_URLCONF='simple_history.tests.urls',
+    MEDIA_ROOT=media_root,
+    STATIC_URL='/static/',
+    INSTALLED_APPS=installed_apps,
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+        }
+    },
+    AUTH_USER_MODEL=auth_user_model
+)
 
 
 def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    if hasattr(django, 'setup'):
+        django.setup()
     from django.test.simple import DjangoTestSuiteRunner
     failures = DjangoTestSuiteRunner(
         verbosity=1, interactive=True, failfast=False).run_tests(['tests'])
