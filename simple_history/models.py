38a7e81a1725dd0c7ad78fe6dca63fc602e63e63
Daniel Roschka <danielroschka@phoenitydawn.de>
2014-03-15 13:03:54 +0100

    Fixed import of datetime.now as noticed by treyhunner.

--- simple_history/models.py
+++ simple_history/models.py
@@ -9,7 +9,8 @@ from django.utils import importlib
 try:
     from django.utils.timezone import now
 except ImportError:
-    from datetime.datetime import now
+    from datetime import datetime
+    now = datetime.now
 from .manager import HistoryDescriptor
 
 try:
