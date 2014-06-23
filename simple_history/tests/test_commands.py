924820be113556d443af6f3cf59a88b79f2f1b67
Micah Denbraver <macromicah@gmail.com>
2014-06-22 17:40:01 -0700

    Added test for specific model repopulation

--- simple_history/tests/test_commands.py
+++ simple_history/tests/test_commands.py
@@ -13,3 +13,12 @@ class TestPopulateHistory(TestCase):
         models.Poll.history.all().delete()
         management.call_command(self.command_name, auto=True)
         self.assertEqual(models.Poll.history.all().count(), 1)
+
+    def test_specific_populate(self):
+        models.Poll.objects.create(question="Will this populate?", pub_date=datetime.now())
+        models.Poll.history.all().delete()
+        models.Book.objects.create(isbn="9780007117116")
+        models.Book.history.all().delete()
+        management.call_command(self.command_name, "tests.Book")
+        self.assertEqual(models.Book.history.all().count(), 1)
+        self.assertEqual(models.Poll.history.all().count(), 0)
