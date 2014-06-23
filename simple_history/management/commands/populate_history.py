924d59e9b4c4f28114a738db4a10c3d42ae241be
Micah Denbraver <macromicah@gmail.com>
2014-06-23 00:42:11 -0700

    Cleanup

--- simple_history/management/commands/populate_history.py
+++ simple_history/management/commands/populate_history.py
@@ -1,25 +1,29 @@
 from __future__ import unicode_literals, print_function
 
 from optparse import make_option
-
 from django.db import models as db_models
 from django.core.management.base import BaseCommand
 from django.contrib.contenttypes.models import ContentType
 
-from simple_history.manager import HistoryDescriptor
+from ...manager import HistoryDescriptor
+from ... import models
 
 
 class Command(BaseCommand):
-    args = '<model model ...>'
-    help = 'Populates the corresponding HistoricalRecords field with the current state of all instances in a model'
+    args = "<app.model app.model ...>"
+    help = ("Populates the corresponding HistoricalRecords field with "
+            "the current state of all instances in a model")
 
     option_list = BaseCommand.option_list + (
-        make_option('--auto',
-            action = 'store_true',
-            dest = 'auto',
-            default = False,
-            help = 'Automatically search for models with the HistoricalRecords field type'),
-        )
+        make_option(
+            '--auto',
+            action='store_true',
+            dest='auto',
+            default=False,
+            help="Automatically search for models with the "
+                 "HistoricalRecords field type",
+        ),
+    )
 
     def handle(self, *args, **options):
         models = db_models.get_models()
@@ -27,11 +31,16 @@ class Command(BaseCommand):
         if args:
             for model in models:
                 content_type = ContentType.objects.get_for_model(model)
-                if "{app}.{model}".format(app=content_type.app_label, model=content_type.model) in args:
+                model_string = "{app}.{model}".format(
+                    app=content_type.app_label,
+                    model=content_type.model,
+                )
+                if model_string in args:
                     if self._check_and_save(model):
                         is_success = True
                     else:
-                        print('HistoricalRecords field does not exist in the model %s.' % model.__name__)
+                        print("HistoricalRecords field does not exist in the "
+                              "model {model}.".format(model=model.__name__))
         elif options['auto']:
             print('Searching for models with the HistoricalRecords field..')
             for model in models:
@@ -45,15 +54,19 @@ class Command(BaseCommand):
             print('Command executed successfully')
 
     def _check_and_save(self, model):
-        ''' Checks if a HistoricalRecords field exists in the model '''
-        ''' Calls the save() method on all of the model's instances '''
-        has_history = False
-        for field in model.__dict__:
-            if type(model.__dict__[field]) == HistoryDescriptor:
-                print('Found HistoricalRecords field in model %s' % model.__name__)
+        """Checks if a HistoricalRecords field exists in the model
+
+        Calls the save() method on all of the model's instances
+
+        """
+        for name, attr in model.__dict__.items():
+            if isinstance(attr, HistoryDescriptor):
+                print("Found HistoricalRecords field "
+                      "in model {model}".format(model=model.__name__))
                 instances = model.objects.all()
                 print('Saving %d instances..' % instances.count())
                 for object in instances:
                     object.save()
-                has_history = True
-        return has_history
+        else:
+            return False
+        return True
--- simple_history/tests/test_commands.py
+++ simple_history/tests/test_commands.py
@@ -10,24 +10,26 @@ from . import models
 
 @contextmanager
 def capture(command, *args, **kwargs):
-  out, sys.stdout = sys.stdout, StringIO()
-  command(*args, **kwargs)
-  sys.stdout.seek(0)
-  yield sys.stdout.read()
-  sys.stdout = out
+    out, sys.stdout = sys.stdout, StringIO()
+    command(*args, **kwargs)
+    sys.stdout.seek(0)
+    yield sys.stdout.read()
+    sys.stdout = out
 
 
 class TestPopulateHistory(TestCase):
     command_name = 'populate_history'
 
     def test_auto_populate(self):
-        models.Poll.objects.create(question="Will this populate?", pub_date=datetime.now())
+        models.Poll.objects.create(question="Will this populate?",
+                                   pub_date=datetime.now())
         models.Poll.history.all().delete()
         with capture(management.call_command, self.command_name, auto=True):
             self.assertEqual(models.Poll.history.all().count(), 1)
 
     def test_specific_populate(self):
-        models.Poll.objects.create(question="Will this populate?", pub_date=datetime.now())
+        models.Poll.objects.create(question="Will this populate?",
+                                   pub_date=datetime.now())
         models.Poll.history.all().delete()
         models.Book.objects.create(isbn="9780007117116")
         models.Book.history.all().delete()
