c024389238d4cb61e96ae5e58c7a52e49a7a1048
Micah Denbraver <macromicah@gmail.com>
2014-06-24 23:09:46 -0700

    Refactored into smaller methods

--- simple_history/management/commands/populate_history.py
+++ simple_history/management/commands/populate_history.py
@@ -1,27 +1,24 @@
-from __future__ import unicode_literals, print_function
+from __future__ import print_function
 
-try:
-    from django.utils.timezone import now
-except ImportError:     # pragma: no cover
-    from datetime import datetime
-    now = datetime.now
 from optparse import make_option
-from django.db import transaction, models as db_models
-from django.core.management.base import BaseCommand
-from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models.loading import get_model
 
-
-COMMAND_HINT = "Please specify a model or use the --auto option"
-
-
-class NotHistorical(TypeError):
-    """No related history model found."""
+from ... import models
+from . import _populate_utils as utils
 
 
 class Command(BaseCommand):
     args = "<app.model app.model ...>"
     help = ("Populates the corresponding HistoricalRecords field with "
             "the current state of all instances in a model")
+
+    COMMAND_HINT = "Please specify a model or use the --auto option"
+    MODEL_NOT_FOUND = "Unable to find model"
+    MODEL_NOT_HISTORICAL = "No history model found"
+    NO_REGISTERED_MODELS = "No registered models were found"
+    START_SAVING_FOR_MODEL = "Starting saving historical records for {model}"
+    DONE_SAVING_FOR_MODEL = "Finished saving historical records for {model}"
 
     option_list = BaseCommand.option_list + (
         make_option(
@@ -35,66 +32,54 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        models = db_models.get_models()
-        is_success = False
+        to_process = set()
+
         if args:
-            for model in models:
-                content_type = ContentType.objects.get_for_model(model)
-                model_string = "{app}.{model}".format(
-                    app=content_type.app_label,
-                    model=content_type.model,
-                )
-                if model_string in args:
-                    try:
-                        self._check_and_save(model)
-                    except NotHistorical:
-                        self.stdout.write(
-                            "HistoricalRecords field does not exist in the "
-                            "model {model}.".format(model=model.__name__)
-                        )
-                    else:
-                        is_success = True
+            for model_pair in self._handle_model_list(*args):
+                to_process.add(model_pair)
+
         elif options['auto']:
-            self.stdout.write("Searching for models with the "
-                              "HistoricalRecords field..")
-            for model in models:
-                try:
-                    self._check_and_save(model)
-                except NotHistorical:
-                    pass
-                else:
-                    is_success = True
-            if not is_success:
-                self.stdout.write("No model with HistoryDescriptor "
-                                  "field was found")
+            for model in models.registered_models.values():
+                try:    # avoid issues with mutli-table inheritance
+                    history_model = utils.get_history_model_for_model(model)
+                except utils.NotHistorical:
+                    continue
+                to_process.add((model, history_model))
+            if not to_process:
+                self.stdout.write(self.NO_REGISTERED_MODELS)
+
         else:
-            self.stdout.write(COMMAND_HINT)
+            self.stdout.write(self.COMMAND_HINT)
 
-    def _check_and_save(self, model):
-        """Look up the historical manager and save a copy of all
-        instances to the historical model.
+        self._process(to_process)
 
-        """
+    def _handle_model_list(self, *args):
+        failing = False
+        for natural_key in args:
+            try:
+                model, history = self._model_from_natural_key(natural_key)
+            except ValueError as e:
+                self.stderr.write(unicode(e))
+            else:
+                if not failing:
+                    yield (model, history)
+        if failing:
+            raise CommandError
+
+    def _model_from_natural_key(self, natural_key):
+        model = get_model(*natural_key.split(".", 1))
+        if not model:
+            raise ValueError(self.MODEL_NOT_FOUND +
+                             " < {model} >".format(model=natural_key))
         try:
-            manager_name = model._meta.simple_history_manager_attribute
-        except AttributeError:
-            raise NotHistorical("Cannot find a historical model for "
-                                "{model}".format(model=model))
-        history_klass = getattr(model, manager_name).model
-        self.stdout.write("Found HistoricalRecords field "
-                          "in model {model}".format(model=model.__name__))
-        historical_instances = [
-            history_klass(
-                history_date=getattr(instance, '_history_date', now()),
-                history_user=getattr(instance, '_history_user', None),
-                **dict((field.attname, getattr(instance, field.attname))
-                   for field in instance._meta.fields)
-            ) for instance in model.objects.all()]
-        self.stdout.write("Saving {count} instances..".format(count=len(historical_instances)))
-        try:
-            history_klass.objects.bulk_create(historical_instances)
-        except AttributeError:  # pragma: no cover
-            # bulk_create was added in Django 1.4, handle legacy versions
-            with transaction.commit_on_success():
-                for instance in historical_instances:
-                    instance.save()
+            history_model = utils.get_history_model_for_model(model)
+        except utils.NotHistorical:
+            raise ValueError(self.MODEL_NOT_HISTORICAL +
+                             " < {model} >".format(model=natural_key))
+        return model, history_model
+
+    def _process(self, to_process):
+        for model, history_model in to_process:
+            self.stdout.write(self.START_SAVING_FOR_MODEL.format(model=model))
+            utils.bulk_history_create(model, history_model)
+            self.stdout.write(self.DONE_SAVING_FOR_MODEL.format(model=model))
--- simple_history/tests/tests/test_commands.py
+++ simple_history/tests/tests/test_commands.py
@@ -13,18 +13,20 @@ class TestPopulateHistory(TestCase):
     def test_no_args(self):
         out = StringIO()
         management.call_command(self.command_name, stdout=out)
-        self.assertIn(populate_history.COMMAND_HINT, out.getvalue())
+        self.assertIn(populate_history.Command.COMMAND_HINT, out.getvalue())
 
     def test_invalid_model(self):
         out = StringIO()
-        management.call_command(self.command_name, "tests.place", stdout=out)
-        self.assertIn("does not", out.getvalue())
+        management.call_command(self.command_name, "tests.place", stderr=out)
+        self.assertIn(populate_history.Command.MODEL_NOT_HISTORICAL,
+                      out.getvalue())
 
     def test_auto_populate(self):
         models.Poll.objects.create(question="Will this populate?",
                                    pub_date=datetime.now())
         models.Poll.history.all().delete()
-        management.call_command(self.command_name, auto=True, stdout=StringIO())
+        management.call_command(self.command_name, auto=True,
+                                stdout=StringIO())
         self.assertEqual(models.Poll.history.all().count(), 1)
 
     def test_specific_populate(self):
@@ -33,6 +35,16 @@ class TestPopulateHistory(TestCase):
         models.Poll.history.all().delete()
         models.Book.objects.create(isbn="9780007117116")
         models.Book.history.all().delete()
-        management.call_command(self.command_name, "tests.book", stdout=StringIO())
+        management.call_command(self.command_name, "tests.book",
+                                stdout=StringIO())
         self.assertEqual(models.Book.history.all().count(), 1)
         self.assertEqual(models.Poll.history.all().count(), 0)
+
+    def test_multi_table(self):
+        data = {'rating': 5, 'name': "Tea 'N More"}
+        models.Restaurant(**data).save_without_historical_record()
+        management.call_command(self.command_name, 'tests.restaurant',
+                                stdout=StringIO())
+        update_record = models.Restaurant.updates.all()[0]
+        for attr, value in data.items():
+            self.assertEqual(getattr(update_record, attr), value)
