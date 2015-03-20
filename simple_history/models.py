101675c2f54ff2084ad9503062b3f44f3129bfaf
Rod Xavier Bondoc <rod@piran.com.au>
2015-03-20 13:49:44 +1100

    Fix do_related_class()

--- simple_history/models.py
+++ simple_history/models.py
@@ -295,15 +295,14 @@ class CustomForeignKeyField(models.ForeignKey):
     def do_related_class(self, other, cls):
         field = self.get_field(other, cls)
         if not hasattr(self, 'related'):
-            try:    
-                self.related = ForeignObjectRel(self, other)
+            try:
+                instance_type = cls.instance_type
+            except AttributeError:  # when model is reconstituted for migration
+                pass  # happens during migrations
+            else:
+                self.related = ForeignObjectRel(self, instance_type)
             except TypeError:  # < Django 1.8
-                try:
-                    instance_type = cls.instance_type
-                except AttributeError:  # when model is reconstituted for migration
-                    pass  # happens during migrations
-                else:
-                    self.related = ForeignObjectRel(other, instance_type, self)
+                self.related = ForeignObjectRel(other, instance_type, self)
         transform_field(field)
         field.rel = None
 
