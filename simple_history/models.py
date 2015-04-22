cd46eddbc3ca95d2e4f8e38bb8420a87ca18af9a
Micah Denbraver <macromicah@gmail.com>
2015-04-21 21:12:27 -0700

    Removed dictionary comprehension for py26 support (woo!)

--- simple_history/models.py
+++ simple_history/models.py
@@ -174,8 +174,8 @@ class HistoricalRecords(object):
                     [getattr(self, opts.pk.attname), self.history_id])
 
         def get_instance(self):
-            return model(**{field.attname: getattr(self, field.attname)
-                            for field in fields.values()})
+            return model(**dict([(field.attname, getattr(self, field.attname))
+                                for field in fields.values()]))
 
         return {
             'history_id': models.AutoField(primary_key=True),
