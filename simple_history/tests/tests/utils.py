343f66fe40fa000923e9270eb77c2c670ff82fcc
Alexander Anikeev <a.anikeev@volsor.com>
2018-06-03 03:40:00 +0700

    Rename variable to fit into line length

--- simple_history/tests/tests/utils.py
+++ simple_history/tests/tests/utils.py
@@ -1,17 +1,17 @@
 import django
 from django.conf import settings
 
-simple_history_middleware = 'simple_history.middleware.HistoryRequestMiddleware'
+request_middleware = 'simple_history.middleware.HistoryRequestMiddleware'
 
 if django.__version__ >= '2.0':
     middleware_override_settings = {
         'MIDDLEWARE': (
-            settings.MIDDLEWARE + [simple_history_middleware]
+            settings.MIDDLEWARE + [request_middleware]
         )
     }
 else:
     middleware_override_settings = {
         'MIDDLEWARE_CLASSES': (
-            settings.MIDDLEWARE_CLASSES + [simple_history_middleware]
+            settings.MIDDLEWARE_CLASSES + [request_middleware]
         )
     }
