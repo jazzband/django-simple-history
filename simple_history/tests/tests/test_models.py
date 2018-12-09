fd1a30bbb7e153bc995c89dd76052e014359e565
ali_cramstack <mazg1493@gmail.com>
2018-12-09 15:52:30 +0600

    Formatted test_models file

--- simple_history/tests/tests/test_models.py
+++ simple_history/tests/tests/test_models.py
@@ -446,10 +446,10 @@ class HistoricalRecordsTest(TestCase):
         self.assertNotIn("pub_date", all_fields_names)
 
         most_recent = p.history.most_recent()
-        self.assertIn('question', all_fields_names)
-        self.assertNotIn('pub_date', all_fields_names)
+        self.assertIn("question", all_fields_names)
+        self.assertNotIn("pub_date", all_fields_names)
         self.assertEqual(most_recent.__class__, PollWithExcludeFields)
-        self.assertIn('pub_date', history._history_excluded_fields)
+        self.assertIn("pub_date", history._history_excluded_fields)
 
     def test_user_model_override(self):
         user1 = User.objects.create_user("user1", "1@example.com")
