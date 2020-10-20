b3fe69727e60ed6dba708d317ad89eaefbd93bad
Jannis Leidel <jannis@leidel.info>
2020-10-20 21:44:32 +0200

    Black fixes.

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 with open("README.rst") as readme, open("CHANGES.rst") as changes:
     setup(
         name="django-simple-history",
-        use_scm_version={
-            "version_scheme": "post-release",
-        },
+        use_scm_version={"version_scheme": "post-release"},
         setup_requires=["setuptools_scm"],
         description="Store model history and view/revert changes from admin site.",
         long_description="\n".join((readme.read(), changes.read())),
--- simple_history/__init__.py
+++ simple_history/__init__.py
@@ -4,8 +4,9 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-     # package is not installed
+    # package is not installed
     pass
+
 
 def register(
     model,
