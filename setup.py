20007fd82d42c0966e786d8f5d5e08e7c7e5e7ce
Hasan Ramezani <hasan.r67@gmail.com>
2021-11-10 01:11:00 +0330

    Add Python 3.10 to test matrix. (#907)
    * Add Python 3.10 to test matrix.
    
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ with open("README.rst") as readme, open("CHANGES.rst") as changes:
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "License :: OSI Approved :: BSD License",
         ],
         python_requires=">=3.6",
