bfc5204bb73e88da215ecc55875d2c8302cd6744
Jim King <jim.king@cloudtruth.com>
2022-04-22 17:20:06 -0400

    fix project metadata to fix install issues

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup
 with open("README.rst") as readme, open("CHANGES.rst") as changes:
     setup(
         name="django-simple-history",
-        use_scm_version={"version_scheme": "post-release"},
+        use_scm_version={
+            "version_scheme": "post-release",
+            "local_scheme": "node-and-date",
+            "relative_to": __file__,
+            "root": ".",
+        },
         setup_requires=["setuptools_scm"],
         description="Store model history and view/revert changes from admin site.",
         long_description="\n".join((readme.read(), changes.read())),
+        long_description_content_type="text/x-rst",
         author="Corey Bertram",
         author_email="corey@qr7.com",
         maintainer="Trey Hunner",
