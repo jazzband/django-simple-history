c9fa8cfcd49e744baa2e37f0b931580ea7c04ba2
areski <areski@gmail.com>
2022-04-29 17:42:40 +0200

    [fix] add fallback_version to scm settings

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open("README.rst") as readme, open("CHANGES.rst") as changes:
             "local_scheme": "node-and-date",
             "relative_to": __file__,
             "root": ".",
+            "fallback_version": "0.0.0",
         },
         setup_requires=["setuptools_scm"],
         description="Store model history and view/revert changes from admin site.",
