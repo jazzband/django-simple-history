5439c006b228fb86f3e0d5f4c0840f1277c8be3a
Anders <6058745+ddabble@users.noreply.github.com>
2023-07-04 20:40:01 +0200

    Replaced usages of the deprecated pkg_resources
    (It seems like after the last commit added `python = 3.12` to `tox.ini`,
    that version was actually being used, which made the tests fail against
    Python 3.12-dev.)
    
    As of Python 3.12, the `pkg_resources` package is no longer provided
    (see https://docs.python.org/3.13/whatsnew/3.12.html#removed).
    The package has been deprecated in favor of `importlib.metadata
    (see the "Attention" notice at the top of
    https://setuptools.pypa.io/en/latest/pkg_resources.html),
    and the replacement for `pkg_resources.get_distribution()` is
    `importlib.metadata.version()` (according to
    https://github.com/googleapis/python-api-core/issues/27#issuecomment-676563754).
    See also https://docs.python.org/3/library/importlib.metadata.html#distribution-versions.

--- simple_history/__init__.py
+++ simple_history/__init__.py
@@ -1,8 +1,8 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib import metadata
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = metadata.version(__name__)
+except metadata.PackageNotFoundError:
     # package is not installed
     pass
 
